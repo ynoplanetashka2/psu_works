e5dee02bdcdcd20480e8536c484c3f50d04e7700
Alexander Ermilov <mrynoplanetashka@gmail.com>
2024-02-04 16:28:04 +0500

    improved applyMatrix types

--- optics-matrix/src/optics-matrix-model/applyMatrix.ts
+++ optics-matrix/src/optics-matrix-model/applyMatrix.ts
@@ -1,5 +1,9 @@
-export function applyMatrix(matrix: number[][], vector: number[]) {
-  const result = new Array(vector.length);
+export function applyMatrix<Vec extends number[]>(
+  matrix: number[][],
+  vector: Vec
+): Vec {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const result: Vec = new Array<number>(vector.length) as any;
   for (let i = 0; i < result.length; ++i) {
     result[i] = vector.reduce((vectorEntry, j) => matrix[i][j] * vectorEntry);
   }
