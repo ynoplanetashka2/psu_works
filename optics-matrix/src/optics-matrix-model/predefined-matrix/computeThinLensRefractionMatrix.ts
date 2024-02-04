adba1cf2868db2c4051c3eec7c65ee146d8ed01b
Alexander Ermilov <mrynoplanetashka@gmail.com>
2024-02-04 18:28:30 +0500

    fixed computeThinLensRefractionMatrix computation resuilt

--- optics-matrix/src/optics-matrix-model/predefined-matrix/computeThinLensRefractionMatrix.ts
+++ optics-matrix/src/optics-matrix-model/predefined-matrix/computeThinLensRefractionMatrix.ts
@@ -1,7 +1,11 @@
-export function computeThinLensRefractionMatrix(radiusOfCurvature: number, n1: number = 1, n2: number = 1) {
-    const coeff = (n1 - n2) / radiusOfCurvature;
-    return [
-        [1, coeff],
-        [0, 1]
-    ]
+export function computeThinLensRefractionMatrix(
+  radiusOfCurvature: number,
+  n1: number = 1,
+  n2: number = 1
+) {
+  const coeff = (n1 - n2) / radiusOfCurvature;
+  return [
+    [1, 0],
+    [coeff, 1],
+  ];
 }
