4b7df71d0a1c1a6f731a599f5e83811ae7753665
alex <mrynoplanetashka@gmail.com>
2024-02-08 10:04:52 +0300

    planar example 1

---
+++ graph/planar.py
@@ -0,0 +1,23 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+def init_G():
+    G = nx.Graph()
+    for i in range(7):
+        G.add_node(i)
+    for i in range(7):
+        G.add_edge(i, (i + 1) % 7)
+    for i in range(7):
+        shift = 2
+        G.add_edge(i, (i + shift) % 7)
+        G.add_edge((i + shift) % 7, (i + 2 * shift) % 7)
+    return G
+def main():
+    G = init_G()
+    is_planar, counterexample = nx.check_planarity(G, True)
+    print(is_planar, counterexample)
+    nx.draw(counterexample, with_labels=True)
+    plt.show()
+
+if __name__ == "__main__":
+    main()
--- numerical_methods/symbol_computation/classes/e2.py
+++ numerical_methods/symbol_computation/classes/e2.py
@@ -3,38 +3,75 @@ import numpy as np
 from matplotlib import pyplot as plt
 import itertools
 
+
 def main():
-    t, eta = sp.symbols('t, eta', real=True)
-    x = sp.Function('x', real=True)(t, eta)
+    # f, g = sp.symbols("f g", cls=sp.Function)
+    # x = sp.symbols("x")
+    # eqs = [sp.Eq(f(x).diff(x), g(x)), sp.Eq(g(x).diff(x), f(x))]
+    # sln = sp.dsolve(eqs, [f(x), g(x)], ics={ f(0): 1, g(2): 3 })
+    # print(sln)
+    # exit()
+    t, eta = sp.symbols("t, eta", real=True)
+    # x = sp.Function('x', real=True)(t, eta)
     # x_ser = sp.series(x, eta, 0, 4)
-    X0, X1, X2 = sp.Function('X0', real=True)(t), sp.Function('X1', real=True)(t), sp.Function('X2', real=True)(t)
-    X_arr = [X0, X1, X2]
+    # X0, X1, X2 = sp.Function('X0', real=True)(t), sp.Function('X1', real=True)(t), sp.Function('X2', real=True)(t)
+    X0, X1, X2 = sp.symbols("X0, X1, X2", cls=sp.Function, real=True)
+    X_arr = [X0(t), X1(t), X2(t)]
     # X0, X1, X2, X3, X4 = sp.symbols('X0, X1, X2, X3, X4', real=True)
-    x_ser = X0 + X1 * eta + X2 * eta**2
+    x_ser = X0(t) + X1(t) * eta + X2(t) * eta**2
     # x_ser = X0
-    formula = sp.diff(x_ser, t, t) + x_ser + eta * x_ser ** 3
+    formula = sp.diff(x_ser, t, t) + x_ser + eta * x_ser**3
     expanded = sp.expand(formula).removeO()
     coeffs = sp.Poly(expanded, eta).coeffs()
     coeffs = coeffs[::-1]
-    # print('0' * 20)
-    # print(f'equations count: {len(coeffs)}')
-    # print(coeffs)
-    # print('-' * 20)
-    # print('0', sp.simplify(coeffs[0].expand()))
-    # print('-' * 20)
-    # print('1', coeffs[1])
-    # print('-' * 20)
-    # print('2', coeffs[2])
 
-    eqs = list(map(lambda coeff: sp.Eq(coeff, 0), coeffs))
-    # soln = sp.solve(itertools.islice(eqs, len(coeffs)), X_arr)
-    # soln = sp.solve((eqs[0], eqs[1], eqs[2]), X_arr, dict=True)
-    # print(list(coeffs[0:3]))
-    # print(X_arr)
-    soln = sp.dsolve(coeffs[0:3], X_arr, x0=(0, 0))
+    # eqs = list(map(lambda coeff: sp.Eq(coeff, 0), coeffs))
+    eqs = coeffs
+    solns = []
+    for eq in eqs:
+        soln = sp.dsolve(
+            eq,
+            X_arr,
+            ics={
+                # X0(0): 1,
+                # X1(0): 1,
+                # X2(0): 1,
+                X0(0): (
+                    -(
+                        sp.diff(X0(t) + X1(t) * eta + X2(t) * eta**2, t, t)
+                        + X1(t) * eta
+                        + X2(t) * eta**2
+                        + eta * (X0(t) + X1(t) * eta + X2(t) * eta**2) ** 3
+                    ).subs(t, 0)
+                    - 1
+                ),
+                X0(t)
+                .diff(t)
+                .subs(t, 0): (
+                    (
+                        sp.diff(X0(t) + X1(t) * eta + X2(t) * eta**2, t, t)
+                        + X1(t) * eta
+                        + X2(t) * eta**2
+                        + eta * (X0(t) + X1(t) * eta + X2(t) * eta**2) ** 3
+                    )
+                    .diff(t)
+                    .subs(t, 0)
+                ),
+                # x_ser.diff(t)(0): 0,
+            },
+        )
+        solns.append(soln)
+    # print(solns)
+    # soln = list(soln)
+    # print(soln, len(soln))
+    exit()
     soln = list(map(lambda sol: sol.rhs, soln))
-    print(soln)
-    x_ser = x_ser.subs((X0, X1, X2), soln)
+    (X0_sub, X1_sub, X2_sub) = soln
+    print(X0_sub)
+    print(X1_sub)
+    print(X2_sub)
+    # x_ser = x_ser.replace((X0, X1, X2), (X0_sub, X1_sub, X2_sub))
+    x_ser = x_ser.subs([(X0, X0_sub), (X1, X1_sub), (X2, X2_sub)])
     print(x_ser)
 
     t_range = np.linspace(0.5, 10, 30)
@@ -49,5 +86,7 @@ def main():
     print(values)
     plt.plot(t_range, values)
     plt.show()
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     main()
