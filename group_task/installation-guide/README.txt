7138b718be6e2d78f2fc09676f2b98ab20417141
alex <mrynoplanetashka@gmail.com>
2024-02-12 09:20:15 +0300

    lfs files added

---
+++ group_task/installation-guide/README.txt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0dadb5aaf31ee8402f8571423d2d4d5cf42dfcd6de24fae95c0799fc40238fa2
+size 565
---
+++ group_task/installation-guide/group-project-arduino-code/group-project-arduino-code.ino
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d0dba5b72ad743603654ab395e6c95c3752f31ecea21d4a42407aa44cfc41264
+size 586
