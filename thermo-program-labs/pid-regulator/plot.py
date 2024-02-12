14b6e524793da97f17fd4ea2bc44776463cdf969
alex <mrynoplanetashka@gmail.com>
2024-02-12 08:58:56 +0300

    add dynamic plot

--- thermo-program-labs/pid-regulator/plot.py
+++ thermo-program-labs/pid-regulator/plot.py
@@ -6,36 +6,35 @@ NavigationToolbar2Tk)
 # plot function is created for 
 # plotting the graph in 
 # tkinter window 
-def plot(window): 
+def plot(window, data): 
 
-	# the figure that will contain the plot 
-	fig = Figure(figsize = (5, 5), 
-				dpi = 100) 
+    # the figure that will contain the plot 
+    fig = Figure(figsize = (5, 5), 
+                dpi = 100) 
 
-	# list of squares 
-	y = [i**2 for i in range(101)] 
+    # adding the subplot 
+    plot1 = fig.add_subplot(111) 
 
-	# adding the subplot 
-	plot1 = fig.add_subplot(111) 
+    # plotting the graph 
+    plot1.plot(data) 
 
-	# plotting the graph 
-	plot1.plot(y) 
+    # creating the Tkinter canvas 
+    # containing the Matplotlib figure 
+    canvas = FigureCanvasTkAgg(fig, 
+                            master = window) 
+    canvas.draw() 
 
-	# creating the Tkinter canvas 
-	# containing the Matplotlib figure 
-	canvas = FigureCanvasTkAgg(fig, 
-							master = window) 
-	canvas.draw() 
+    # placing the canvas on the Tkinter window 
+    tk_widget = canvas.get_tk_widget() 
+    tk_widget.pack()
 
-	# placing the canvas on the Tkinter window 
-	canvas.get_tk_widget().pack() 
+    # creating the Matplotlib toolbar 
+    toolbar = NavigationToolbar2Tk(canvas, 
+                                window) 
+    toolbar.update() 
 
-	# creating the Matplotlib toolbar 
-	toolbar = NavigationToolbar2Tk(canvas, 
-								window) 
-	toolbar.update() 
-
-	# placing the toolbar on the Tkinter window 
-	canvas.get_tk_widget().pack() 
+    # placing the toolbar on the Tkinter window 
+    canvas.get_tk_widget().pack() 
+    return (plot1, canvas)
 
 
--- thermo-program-labs/pid-regulator/setup_window.py
+++ thermo-program-labs/pid-regulator/setup_window.py
@@ -1,19 +1,21 @@
 import tkinter as tk
+
+import numpy as np
 from plot import plot
 
-TIMER_INTERVAL = 300
-TAU_0 = 50
-TEMP_0 = 10
+TIMER_INTERVAL = 30
+TAU_0 = 30
+TEMP_0 = 100
 T_INI = 0
-T_SP = 30
-HE = 0.2
+T_SP = 5
+HE = 0.0
 
 def iteration(T, delta_t, p_relative):
-    delta_T = (p_relative * T_SP / TAU_0 - T / TAU_0) * delta_t
+    delta_T = (p_relative * TEMP_0 / TAU_0 - T / TAU_0) * delta_t
     return T + delta_T
 
 def compute_p(T):
-    if T >= T_SP + HE:
+    if T >= T_SP - HE:
         return 0
     return 1
 
@@ -21,14 +23,21 @@ def setup_window():
     window = tk.Tk()
     lbl = None
     T_value = T_INI
+    T_all_values = [T_value]
+    (ax, canvas) = plot(window, T_all_values)
 
     def timer_tick():
         nonlocal T_value
         p_relative = compute_p(T_value)
         res = iteration(T_value, TIMER_INTERVAL / 1000, p_relative)
         T_value = res
+        T_all_values.append(T_value)
         lbl.config(text=res)
-        plot(window)
+        domain = np.arange(len(T_all_values)) * TIMER_INTERVAL
+        ax.plot(domain, T_all_values, color='blue')
+        ax.set_xlim((max(0, domain[-1] - 10 * 1000), domain[-1]))
+        canvas.draw()
+        window.update()
         window.after(TIMER_INTERVAL, timer_tick)
     lbl = tk.Label()
     lbl.pack()
