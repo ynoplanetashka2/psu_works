e74811be095c0d905cee5bd86dfb77bd7c815ae2
alex <mrynoplanetashka@gmail.com>
2024-02-22 09:48:07 +0300

    signal-analysis plot multiple llines

--- signal_analysis/sin.py
+++ signal_analysis/sin.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 from src.fourier import fourier, fourier_freq, fourier_sq, real_fourier
 
-SIGMA = 10
+SIGMA = 13
 FREQ = 5
 def main():
     #continuous example
@@ -12,15 +12,22 @@ def main():
     identity = lambda x: x
     exp = lambda x: np.exp(-x)
     func = lambda x: np.exp(-(x ** 2 / SIGMA)) * np.sin(FREQ * x)
+    func2 = lambda x: np.exp(-(x ** 2 / SIGMA * 3)) * np.sin(FREQ * x)
     transformed_sin = continuous_fourier(sin)
     transformed_identity = continuous_fourier(identity)
     transformed_exp = continuous_fourier(exp)
     transformed_func = continuous_fourier(func)
+    transformed_func2 = continuous_fourier(func2)
     freq = np.linspace(1, 10, 100)
     values = np.array([np.abs(transformed_func(arg)) for arg in freq])
-    plt.plot(freq, values * 10)
-    # domain = np.linspace(0, 10, 100)
-    # plt.plot(domain, [func(val) for val in domain])
+    values2 = np.array([np.abs(transformed_func2(arg)) for arg in freq])
+    # plt.plot(freq, values * 10)
+    # plt.plot(freq, values2 * 10)
+    domain = np.linspace(0, 10, 100)
+    plt.plot(domain, [func(val) for val in domain])
+    plt.plot(domain, [func2(val) for val in domain])
+    # plt.xscale("log")
+    # plt.yscale("log")
     plt.show()
     exit()
     #discrete example
