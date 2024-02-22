5b45d64bb1c10562396d97daf840b6d0eddd5a8b
Alexander Ermilov <mrynoplanetashka@gmail.com>
2024-02-22 06:05:01 +0500

    add continuous_fourier exampe plot

--- signal_analysis/sin.py
+++ signal_analysis/sin.py
@@ -1,9 +1,29 @@
 import numpy as np
+from src.continuous_fourier import continuous_fourier
 import matplotlib.pyplot as plt
 
 from src.fourier import fourier, fourier_freq, fourier_sq, real_fourier
 
+SIGMA = 10
+FREQ = 5
 def main():
+    #continuous example
+    sin = lambda x: np.sin(x)
+    identity = lambda x: x
+    exp = lambda x: np.exp(-x)
+    func = lambda x: np.exp(-(x ** 2 / SIGMA)) * np.sin(FREQ * x)
+    transformed_sin = continuous_fourier(sin)
+    transformed_identity = continuous_fourier(identity)
+    transformed_exp = continuous_fourier(exp)
+    transformed_func = continuous_fourier(func)
+    freq = np.linspace(1, 10, 100)
+    values = np.array([np.abs(transformed_func(arg)) for arg in freq])
+    plt.plot(freq, values * 10)
+    # domain = np.linspace(0, 10, 100)
+    # plt.plot(domain, [func(val) for val in domain])
+    plt.show()
+    exit()
+    #discrete example
     _args = np.linspace(0, 10, 1000)
     _sin = np.sin(_args * 2 * np.pi)
     _sin = _sin - _sin.mean()
