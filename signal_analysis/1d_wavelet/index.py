84ff51e3a6981ed65ff2bcbffea6d2feceb92dd9
Alexander Ermilov <mrynoplanetashka@gmail.com>
2024-04-17 19:30:51 +0500

    add new 1d wavelet plot

--- signal_analysis/1d_wavelet/index.py
+++ signal_analysis/1d_wavelet/index.py
@@ -6,16 +6,13 @@ import pywt as wt
 
 def main():
     domain = np.arange(480)
-    left_half  = domain[:250]
-    right_half = domain[250:]
-    left_values = np.sin(2 * np.pi * left_half / 32)
-    right_values = np.sin(3 * np.pi * right_half / 32)
-    values = np.concatenate((left_values, right_values))
-    widths = np.arange(1, 46)
+    values = (
+        np.sin((0.5 + domain / 1000) * np.pi * domain / 8)
+        + np.sin((1 - domain / 1000) * np.pi * domain / 8)
+    )
+    widths = np.arange(1, 80)
     cwmatr, freqs = wt.cwt(values, widths, "morl")
-    # plt.imshow(cwmatr, extent=[-1, 1, 1, 31], cmap="PRGn", aspect="auto")
     plt.matshow(cwmatr)
-    # plt.plot(coefs, freqs)
     plt.show()
 
 if __name__ == "__main__":
